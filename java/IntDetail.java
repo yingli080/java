b8197915a93ad20d16f16fc6daccfab661351079
ying.litj@outlook.com <Sky27917917>
2023-07-01 14:35:07 +0900

    hanshunping

---
+++ java/IntDetail.java
@@ -0,0 +1,14 @@
+package Han.java;
+
+public class IntDetail {
+	public static void main(String[] args) {
+		
+		//Java的整数常量（具体值）默认为int型，声明long型常量须后加‘1’或‘L’
+		int n1 = 1;//4个字节
+		//int n2 = 1L;//对不对？是不对的
+
+	
+		
+	}
+
+}
